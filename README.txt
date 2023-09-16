687c2d55ad0a912701492584842cdaed11e62914
Nick Rafala <njrafala@outlook.com>
2023-09-16 14:26:36 -0400

    Initial commit with crossplatform socket function

---
+++ README.txt
@@ -0,0 +1,8 @@
+Description:
+This is a system design project that will have a server and client. Servers will be able to scale horizontally to handle requests from a large number of clients. A load balancer will be used to direct client requests to reduce strain on any given server. Servers will have access to a database for persistent data storage.
+
+Technologies:
+Most of the project will be written in c++ where the server and load balancer will be console applications and the client will be a Qt project. Additional client may be created in the future such as an android app written in kotlin. Servers will access a MySQL database and if clients end up needing a database to store data locally then SQLite will be used.
+
+Crossplatform compatibility:
+The server and load balancer will use preprocessor directives to allow the use of both Windows and Berkley sockets so that they can run on any platform. Qt is able to compile on both Windows and Linux, which allows the client application to run on either platform. A kotlin multiplatform project may also be created to bring a client app to both android and iOS with one codebase written in kotlin.
---
+++ TheSystemServer/TheSystemServer/sockets.cpp
@@ -0,0 +1,63 @@
+
+#include "sockets.h"
+
+SOCKET createSocket(int domain, int type, int protocol) {
+#if defined(_WIN32)
+	WSADATA d;
+	static bool winsockInitialized = false;
+	if (!winsockInitialized) {
+		if (WSAStartup(MAKEWORD(2, 2), &d)) {
+			std::cout << "Failed to initialize WSA" << std::endl;
+			return -1;
+		} else {
+			winsockInitialized = true;
+		}
+	}
+#endif
+
+	return socket(domain, type, protocol);
+}
+
+void closeSocket(SOCKET sock) {
+#if defined(_WIN32)
+	closesocket(sock);
+#else
+	close(sock);
+#endif
+}
+
+int getSocketErrno() {
+#if defined(_WIN32)
+	return WSAGetLastError();
+#else
+	return errno;
+#endif
+}
+
+void printErrorText(int error) {
+#if defined(_WIN32)
+	LPWSTR message = nullptr;
+	const DWORD MESSAGE_BUFFER_SIZE = 256;
+	FormatMessageW(
+		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		0, error, 0, message, MESSAGE_BUFFER_SIZE, 0);
+
+	printf("%S\n", message);
+#else
+
+	printf("%s\n", strerror(error));
+
+#endif
+}
+
+bool isValidSocket(SOCKET sock) {
+	return (INVALID_SOCKET != sock);
+}
+
+struct sockaddr_in makeSockaddr(ADDRESS_FAMILY family, const char *address, USHORT port) {
+	struct sockaddr_in addr;
+	addr.sin_family = family;
+	addr.sin_port = port;
+	inet_pton(family, address, &(addr.sin_addr.s_addr));
+	return addr;
+}
